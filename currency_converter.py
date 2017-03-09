8600c642927b4cb1a8d37e3dd79dd9e04eb6cb5f
Jan Spacek <jan.spacek12@gmail.com>
2017-03-09 01:01:13 +0100

    CheckCurrencyInput function added

--- currency_converter.py
+++ currency_converter.py
@@ -2,6 +2,9 @@ import argparse
 import requests
 
 def provideArguments():
+    '''
+    Sets up and returns arguments for the application
+    '''
     parser = argparse.ArgumentParser(description='Convert currencies')
     requiredArgs = parser.add_argument_group('required arguments')
     requiredArgs.add_argument('--amount', help='Amount to be Converted', required=True, type=float)
@@ -11,7 +14,10 @@ def provideArguments():
     return (args.amount, args.input_currency, args.output_currency)
 
 
-def handleInputs():
+def checkCurrencyInput(currency):
+    '''
+    Checks if specified string is currency symbol or triname
+    '''
     symbols = { "$": "USD",
                 "¥": "CNY",
                 "£": "GBP",
@@ -19,58 +25,61 @@ def handleInputs():
                 "€": "EUR",
                 "₽": "RUB" }
 
+    if len(currency) == 1:
+        if currency in symbols:
+            currency = symbols[currency]
+        else:
+            return 0
+    elif len(currency) == 3:
+        currency = currency.upper()
+    else:
+        return 0
+    return currency
+
+
+def handleInputs():
+    '''
+    Checks provided inputs and returns them
+    '''
     # Gather values from the input arugemnts
     amount, input_currency, output_currency = provideArguments()
-
-
     if amount <= 0:
         print("Invalid amount provided")
         return 0
     else:
         # Check input currency
-        if len(input_currency) == 1:
-            if input_currency in symbols:
-                input_currency = symbols[input_currency]
-            else:
-                print("Input currency symbol not found")
-                return 0
-        elif len(input_currency) == 3:
-            input_currency = input_currency.upper()
-        else:
-            print("Please specify symbol or currency triname to enter input currency")
+        input_currency = checkCurrencyInput(input_currency)
+        if not input_currency:
+            print("Input currency symbol not found or not triname entered")
             return 0
-        # Check output currency
+        # Check output currency (default is None)
         if output_currency:
-            if len(output_currency) == 1:
-                if output_currency in symbols:
-                    output_currency = symbols[output_currency]
-                else:
-                    print("Output currency symbol not found")
-                    return 0
-            elif len(output_currency) == 3:
-                output_currency = output_currency.upper()
-            else:
-                print("Please specify symbol or currency triname to enter output currency")
+            output_currency = checkCurrencyInput(output_currency)
+            if not output_currency:
+                print("Output currency symbol not found or not triname entered")
                 return 0
         # Check if not the same currency provided
         if input_currency == output_currency:
             print("You can't convert to the same currency")
             return 0
-
     return (amount, input_currency, output_currency)
 
 
-
-# Gets json with currency rates
 def getCurrencyJson(input_currency):
+    '''
+    Gets json with currency rates using external API
+    '''
     url = 'https://api.fixer.io/latest?base=' + input_currency
     response = requests.get(url)
     if "error" in response.json():
         return 0
     return response.json()
 
-# Prepares output json
+
 def formatOutput(amount, input_currency):
+    '''
+    Prepares output json
+    '''
     out_json = {}
     out_json['input'] = {}
     out_json['output'] = {}
@@ -78,12 +87,16 @@ def formatOutput(amount, input_currency):
     out_json['input']['currency'] = input_currency
     return out_json
 
-# Converts currencies and updates output json
+
 def convert(amount, output_currency, currency_json, out_json):
+    '''
+    Converts currencies and updates output json
+    '''
+    # update out_json with all the currencies
     if not output_currency:
         for key in currency_json['rates']:
             out_json['output'][key] = round(amount * float(currency_json['rates'][key]), 4)
-
+    # update only with specified currency
     elif output_currency in currency_json['rates']:
         out_json['output'][output_currency] = round(amount * float(currency_json['rates'][output_currency]), 4)
     else:
@@ -95,6 +108,7 @@ def convert(amount, output_currency, currency_json, out_json):
 def main():
 
     try:
+        # handleInputs returns inputs or 0
         amount, input_currency, output_currency  = handleInputs()
     except TypeError:
         return 0
